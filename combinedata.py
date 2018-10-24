ac757a7a8118397e6a806e2fdc1707a52274bf27
Simo Korkolainen <simo.korkolainen@gmail.com>
2018-10-24 15:41:35 +0300

    Arvojen ennustaminen

--- combinedata.py
+++ combinedata.py
@@ -117,11 +117,75 @@ def getCombinedData(startDate, weeks):
 
                 data = pd.concat((data, combined), axis = 0)
 
+
+                
+
         return data
 
 
-def example():
 
+def combinePredictionPointsWithWeather():
+
+        startDate = datetime.datetime(2018, 8, 1)
+        weeks = 1
+
+        selectTime = 1533103200
+        hour = 9
+        year = startDate.year
+        day = startDate.toordinal() + 1 - datetime.datetime(year, 1, 1).toordinal()
+
+
+        road = pd.read_csv('road_shape_prediction_points.csv')
+
+        data = road.copy()
+
+        data['hour'] = hour
+        data['year'] = year
+        data['day'] = day
+
+        names, stationCoord, fmisids = stations.getWeatherStationLocations()
+
+        stationCoord = np.asarray(stationCoord).astype('float')
+
+
+	sensorCoord = road[['longitude', 'latitude']].values
+
+	predictionId = road['prediction_point_id'].values
+	
+        nearest = stations.getNearestWeatherStation(sensorCoord, stationCoord)
+
+	uniqueNearest = np.unique(nearest)
+
+	fmisids = np.asarray(fmisids)
+	
+	uniqueFmisids = fmisids[uniqueNearest]
+
+
+	weather = getWeatherDataFrames(startDate, weeks, uniqueFmisids)
+        for station in uniqueNearest:
+                
+
+                params = ["t2m", "ws_10min", "wg_10min", "wd_10min", "rh", "td", "r_1h", "ri_10min", "snow_aws", "vis", "n_man"]
+
+		print(names[station])
+		print(fmisids[station])
+
+                w = weather[fmisids[station]]
+
+                pos = nearest == station
+                selectedWeather = w[w['unixtime'] == selectTime]
+
+                for col in selectedWeather.columns.values:
+
+                        data.at[pos, col] = selectedWeather[col].values[0]
+
+
+                data.to_csv('prediction_combined.csv', index = False)
+        print(weather)
+
+
+def example():
+        '''
         startDate = datetime.date(2015, 1, 1)
         weeks = 2
 
@@ -129,6 +193,6 @@ def example():
 
         data.to_csv('combined.csv', index = False)
 	
-
-
+        '''
+        combinePredictionPointsWithWeather()
 example()
--- learning.py
+++ learning.py
@@ -7,6 +7,7 @@ import numpy as np
 
 import matplotlib.pyplot as plot
 
+import json
 
 
 import lightgbm as lgb
@@ -18,35 +19,15 @@ import lightgbm as lgb
 
 
 
-def getTarget(data):
-
-        N = data.shape[0]
-        S = np.zeros((N))
-        A = np.zeros((N))
-
-
-        for i in range(90):
-
-
-                S += data['velocity%d' % (i + 111)].values
-        for i in range(200):
-
-
-                A += data['velocity%d' % (i + 1)].values
-
-        
-
-        target = S / (A + 0.1)
-
-        return target
 
 
 def dropColumnsFromData(data, dropSpaceTime):
         for i in range(200):
                 data.drop('velocity%d' % (i + 1), axis = 1, inplace = True)
+        data.drop('speeding', axis = 1, inplace = True)
 
         if dropSpaceTime:
-                dropCols = ['unixtime', 'day', 'year', 'hour', 'id', 'latitude', 'longitude']
+                dropCols = ['unixtime', 'day', 'year', 'hour', 'id', 'station_id', 'latitude', 'longitude']
 
                 data.drop(dropCols, axis = 1, inplace = True)
 
@@ -58,15 +39,36 @@ def getData():
 
         data = pd.read_csv('combined.csv')
 
-        target = getTarget(data)
+        target = np.log1p(data['speeding'].values)
 
+ 
         dropColumnsFromData(data, True)
+
+        data.sort_index(axis = 1, inplace = True)
+
+        print(data.columns)
 
         data = data.values
 
+
         return data, target
 
+def getPredictionData():
 
+        data = pd.read_csv('prediction_combined.csv')
+
+        dataIndex = data['prediction_point_id'].values
+
+        dropCols = ['unixtime', 'day', 'year', 'hour', 'prediction_point_id', 'latitude', 'longitude']
+
+        data.drop(dropCols, axis = 1, inplace = True)
+
+        data.sort_index(axis = 1, inplace = True)
+
+        print(data.columns)
+        data = data.values
+
+        return data, dataIndex
 
 
 def runLightGBM(trainData, trainTarget, testData, testTarget):
@@ -75,7 +77,7 @@ def runLightGBM(trainData, trainTarget, testData, testTarget):
         "metric" : "rmse",
         "task": "train",
         "num_leaves" :20,
-        "learning_rate" : 0.005,
+        "learning_rate" : 0.2,
         "bagging_fraction" : 0.1,
         "feature_fraction" : 1,
         "bagging_freq" : 0,
@@ -99,8 +101,8 @@ def runLightGBM(trainData, trainTarget, testData, testTarget):
 
 def runTraining():
         data, target = getData()
-
         
+        models = []
         
         foldN = 5
         fold = np.random.randint(foldN, size = data.shape[0])
@@ -119,7 +121,7 @@ def runTraining():
                 model, evals_result = runLightGBM(trainData, trainTarget, testData, testTarget)
 
                 pred[fold == i] = model.predict(testData, num_iteration = model.best_iteration)
-                
+                models.append(model)
                 #print(model.feature_importance())
 
 
@@ -131,6 +133,62 @@ def runTraining():
         plot.title('Prediction vs. target')
         
         plot.show()
+        return models
 
 
-runTraining()
+def makePredictions(data, models):
+
+
+        predicted = np.zeros((data.shape[0]))
+
+        for m in models:
+
+
+
+
+
+                pred = m.predict(data, num_iteration = m.best_iteration)
+
+
+                predicted += pred / len(models)
+
+
+        
+        return predicted
+
+
+def writePredictions(models):
+
+        data, dataIndex = getPredictionData()
+        
+        
+        pred = makePredictions(data, models)
+
+        jsonData = None
+
+        with open('map-viewer/data/prediction_points.json') as f:
+                jsonData = json.load(f)
+
+        if jsonData is None:
+                return
+
+
+        for features in jsonData['features']:
+
+                pointId = features['id']
+
+                value = pred[dataIndex == pointId][0]
+
+                features['properties']['predictionResultLog1p'] = value
+
+                features['properties']['predictionResult'] = np.expm1(value)
+
+        with open('map-viewer/data/prediction_points_updated.json', 'w') as output:
+            json.dump(jsonData, output)
+
+
+        
+
+models = runTraining()
+
+writePredictions(models)
